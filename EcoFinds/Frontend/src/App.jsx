84c002fa4651989fbdc5ec7d597b505cefba4a57
mayur2410-tech <mayurwaykar9@gmail.com>
2025-09-06 10:01:38 +0530

    add both frontend and backend

---
+++ EcoFinds/Frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <div className="flex h-screen items-center justify-center bg-blue-500 text-white text-3xl font-bold">
+      Hello Tailwind + Vite 🚀
+    </div>
+    </div>
+  )
+}
+
+export default App
